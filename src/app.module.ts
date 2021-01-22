b69c1631e7c8ca48b5d2b7bc0befd27878b47d17
Qi <904199076@qq.com>
2021-01-22 10:55:32 +0800

    init

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+// import { GraphQLModule } from '@nestjs/graphql';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    // GraphQLModule.forRoot({
+    //   debug: false,
+    //   playground: false,
+    // }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
