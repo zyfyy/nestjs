519d41a97463346c420ac29f932a9c4d4de24bbc
Qi <904199076@qq.com>
2021-01-26 14:30:09 +0800

    add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 // import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { CatsService } from './cats/cats.service';
+
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -12,6 +16,17 @@ import { CatsService } from './cats/cats.service';
     //   debug: false,
     //   playground: false,
     // }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'test',
+      password: 'lol@12Txt_!',
+      database: 'test',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
   ],
   controllers: [AppController, CatsController],
   providers: [AppService, CatsService],
