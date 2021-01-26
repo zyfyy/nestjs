c2d16f77f5ee44894dc012ff6c8be39eb2fbdf48
Qi <904199076@qq.com>
2021-01-26 17:32:33 +0800

    create with mongoose

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 
 import { Cat } from './schemas/cat.schema';
 import { CatsService } from './cats.service';
@@ -6,9 +6,23 @@ import { CatsService } from './cats.service';
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
+  private readonly logger = new Logger(CatsController.name);
 
   @Get()
   async getAllCats(): Promise<Cat[]> {
     return this.catsService.findAll();
+  }
+
+  @Get('create')
+  async createCat(): Promise<Cat> {
+    this.logger.debug('create cat...');
+    this.logger.log('create cat...');
+    this.logger.warn('create cat...');
+    this.logger.error('create cat...');
+    return this.catsService.create({
+      name: 'cat1',
+      age: 12,
+      breed: 'dx',
+    });
   }
 }
